f4185659022570a0f900d81074caf5e7d4527a09
Karthika <karthikakl2000@gmail.com>
2025-05-26 23:35:55 +0530

    updatedUser

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,7 +1,7 @@
-import {upsertStreamUser} from '../lib/stream.js'
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
-
+import cloudinary from "../lib/cloudinary.js";
 
 export async function signUp(req, res) {
   const { fullName, email, password } = req.body;
@@ -35,7 +35,7 @@ export async function signUp(req, res) {
         email,
         password,
         profilepic: randomAvatar,
-      }); 
+      });
 
       const token = jwt.sign(
         { userId: newUser._id },
@@ -75,13 +75,9 @@ export async function login(req, res) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    const token = jwt.sign(
-      { userId: user._id },
-      process.env.JWT_SECRET_KEY,
-      {
-        expiresIn: "7d",
-      }
-    );
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
@@ -91,52 +87,67 @@ export async function login(req, res) {
 
     res.status(201).json({ success: true, user });
   } catch (error) {
-    console.log(('Error in login controller',error.message))
+    console.log(("Error in login controller", error.message));
     res.status(500).json({ message: "Internal server error." });
   }
 }
 
 export function logout(req, res) {
-  res.clearCookie('jwt')
-  res.status(200).json({success:true,message:'Logout successful'})
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logout successful" });
 }
 
-export async function onboard(req,res){
+export async function onboard(req, res) {
   try {
-    const userId=req.user._id
+    const userId = req.user._id;
 
-    const {fullName,bio,location} =req.body
+    const { profilepic, fullName, bio, location } = req.body;
 
-    if(!fullName || !bio || !location){
-      return res.status(400).json({message:'All fields are required.',
-        missingFields:[
-          !fullName&&"fullName",
-          !bio&&"bio",
-          !location&&"location",
-        ].filter(Boolean)
-      })
+    if (!fullName || !bio || !location) {
+      return res
+        .status(400)
+        .json({
+          message: "All fields are required.",
+          missingFields: [
+            !fullName && "fullName",
+            !bio && "bio",
+            !location && "location",
+          ].filter(Boolean),
+        });
     }
-   const updatedUser = await User.findByIdAndUpdate(userId,{
-    ...req.body,
-    OnBoarded :true
-   },{new:true})
+    let updatedUser;
+    if (!profilepic) {
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        {
+          fullName,
+          bio,
+          location,
+          OnBoarded: true,
+        },
+        { new: true }
+      );
+    } else {
+      const uploadResult = await cloudinary.uploader.upload(profilepic);
 
-   if(!updatedUser){
-    return res.status(404).json({message:'User not found'})
-   }
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        {
+          fullName,
+          bio,
+          location,
+          profilepic: uploadResult.secure_url,
+          OnBoarded: true,
+        },
+        { new: true }
+      );
+    }
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-   try {
-    await upsertStreamUser({
-      id:updatedUser._id.toString(),
-      name:updatedUser.fullName,
-      image:updatedUser.profilepic||''
-    })
-    console.log(`Stream user updated after onboearding for ${updatedUser.fullName}`);
-   } catch (streamError) {
-    console.log('Error updating the stream user during onboarding:',streamError.message);
-   }
-   return res.status(200).json({success:true,user:updatedUser});
+    return res.status(200).json({ success: true, user: updatedUser });
   } catch (error) {
-    res.status(500).json({message:'Internal Server Error'});
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
